55a04dddd0f419aea0b2a53e7ce7d7c88869e787
Erik Aakre <erikaakre@hotmail.com>
2023-07-22 17:03:19 -0500

    clean up index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
+// Variables for user input
 let min = 1;
 let max = 100;
 let company = "SCHEELS";
 let extension = ".COM";
 
-const introSection = document.querySelector(".intro");
-
 // Select input buttons and output area for data
+const introSection = document.querySelector(".intro");
 const inputs = document.querySelector(".inputs");
 const inputsRun = inputs.querySelector("#inputs__run");
 const inputsReset = inputs.querySelector("#inputs__reset");
@@ -13,6 +13,23 @@ const output = document.querySelector(".output");
 const outputList = output.querySelector(".output__list");
 const outputListItems = output.querySelectorAll(".output__list-item");
 
+// select modal features for About button/link
+const infoModal = document.getElementById("info-modal");
+const infoButton = document.querySelector(".header__info");
+const infoClose = document.getElementById("info-close");
+
+// Select elements for Settings modal
+const settingsModal = document.getElementById("settings-modal");
+const settingsButton = document.querySelector(".header__settings");
+const settingsClose = document.getElementById("settings-close");
+const settingsForm = document.querySelector(".form");
+const settingsMin = document.getElementById("min");
+const settingsMax = document.getElementById("max");
+const settingsCompany = document.getElementById("company");
+const settingsExtension = document.getElementById("extension");
+const settingsFormError = document.querySelector(".form__error");
+
+// FUNCTIONS
 // function to print number on new line in index.html
 function addItem(text, location) {
   const listItem = document.createElement("li");
@@ -29,6 +46,30 @@ function resetOutput() {
   outputList.textContent = "";
 }
 
+// function to open About modal
+function openAbout() {
+  infoModal.style.display = "flex";
+}
+
+// funtion to open Settings modal
+function openSettings() {
+  settingsModal.style.display = "flex";
+  settingsMin.value = min;
+  settingsMax.value = max;
+  settingsCompany.value = company;
+  settingsExtension.value = extension;
+}
+
+// function to check for valid number inputs
+function checkNumbers(num1, num2) {
+  if (num1 <= num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// EVENT LISTENERS AND FUNCTION CALLS
 // loop through numbers and determine output
 inputsRun.addEventListener("click", () => {
   outputList.textContent = "";
@@ -48,64 +89,25 @@ inputsRun.addEventListener("click", () => {
   }
 });
 
-// call reset function on button click
+// call reset function on "Reset" button click
 inputsReset.addEventListener("click", resetOutput);
-
-// select modal features for About button/link
-const infoModal = document.getElementById("info-modal");
-const infoButton = document.querySelector(".header__info");
-const infoClose = document.getElementById("info-close");
-
-// function to open About modal
-function openAbout() {
-  infoModal.style.display = "flex";
-}
-
-// funtion to open Settings modal
-function openSettings() {
-  settingsModal.style.display = "flex";
-  settingsMin.value = min;
-  settingsMax.value = max;
-  settingsCompany.value = company;
-  settingsExtension.value = extension;
-}
 
 // call function to open About modal via icon
 infoButton.addEventListener("click", openAbout);
 
-// close About modal through close button click
+// close About modal through "Close" button click
 infoClose.addEventListener("click", () => {
   infoModal.style.display = "none";
 });
 
-// Select elements for Settings modal
-const settingsModal = document.getElementById("settings-modal");
-const settingsButton = document.querySelector(".header__settings");
-const settingsClose = document.getElementById("settings-close");
-const settingsForm = document.querySelector(".form");
-const settingsMin = document.getElementById("min");
-const settingsMax = document.getElementById("max");
-const settingsCompany = document.getElementById("company");
-const settingsExtension = document.getElementById("extension");
-const settingsFormError = document.querySelector(".form__error");
-
 // call function to open Settings modal via icon
 settingsButton.addEventListener("click", openSettings);
 
-// close Settings modal through close button click
+// close Settings modal through "Close" button click
 settingsClose.addEventListener("click", () => {
   settingsModal.style.display = "none";
   settingsFormError.style.display = "none";
 });
-
-// function to check for valid number inputs
-function checkNumbers(num1, num2) {
-  if (num1 <= num2) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 // save and submit new settings on Settings modal
 settingsForm.addEventListener("submit", (e) => {
